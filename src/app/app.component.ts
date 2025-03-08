9ded9264179ffb7a4a1627b118e0c12e06cda8b5
xuzhi <compro.xwei@gmail.com>
2025-03-08 17:24:32 -0600

    App finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,60 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  onButtonClick(){
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+  getPassword(){
+    return this.password;
+  }
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+  length = 0;
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }else{
+      this.length = 0;
+    }
+  }
 }
